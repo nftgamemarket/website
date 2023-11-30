0bea573312a8756b369b356d35795a723b18b96d
nftgamemarket <150746348+nftgamemarket@users.noreply.github.com>
2023-11-30 04:04:36 +0000

    added some logic and some css

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
+def home():
+    return render_template('home.html')
 
-def hello():
-    return "Hello World!"
+@app.route("/game/mir4")
+def game_market_list():
+    return render_template('gamemarket_main_page.html')
 
 if __name__ == "__main__":
     app.run(
