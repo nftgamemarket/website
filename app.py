78d70c3a58a9fed54c90e6802df9351237fb3dd4
nftgamemarket <150746348+nftgamemarket@users.noreply.github.com>
2023-12-07 05:02:58 +0000

    novos css e html

--- app.py
+++ app.py
@@ -6,9 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route("/game/mir4")
+@app.route("/games/mir4")
 def game_market_list():
     return render_template('gamemarket_main_page.html')
+
+@app.route("/crop")
+def crop_skill_image():
+    return render_template('crop_image.html')
+
 
 if __name__ == "__main__":
     app.run(
