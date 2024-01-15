6138781d4fb1b0673f0e0ddd52dc0bcf6dfe8f2f
nftgamemarket <150746348+nftgamemarket@users.noreply.github.com>
2024-01-15 18:41:53 +0000

    testes de css

--- app.py
+++ app.py
@@ -2,21 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return render_template('home.html')
+  return render_template('home.html')
+
 
 @app.route("/games/mir4")
 def game_market_list():
-    return render_template('gamemarket_main_page.html')
+  return render_template('gamemarket_main_page.html')
+
+
+@app.route("/css")
+def css():
+  return render_template('game_items_list_bkp.html')
+
 
 @app.route("/crop")
 def crop_skill_image():
-    return render_template('crop_image.html')
+  return render_template('crop_image.html')
 
 
 if __name__ == "__main__":
-    app.run(
-      host = '0.0.0.0',
-      debug = True
-    )
+  app.run(host='0.0.0.0', debug=True)
